f7cbc6f17278cbfc794d8866438c7f0f896a329a
Aniela_Nel <aniela.opolski@gmail.com>
2018-03-08 22:40:47 -0500

    found some used methods in Internetharacteristics I think we can use

--- CloudAnalyst/source/atlas/TrafficMonitor.java
+++ CloudAnalyst/source/atlas/TrafficMonitor.java
@@ -1,5 +1,6 @@
 package atlas;
 
+import cloudsim.ext.InternetCharacteristics;
 // this class will monitor interactions between VM instances at single 
 //data centers to find out what the distribution of users is
 
@@ -7,8 +8,16 @@ package atlas;
 public class TrafficMonitor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// general things we can use:
 
+		// instance object which has traffic levels map and has useful stuffs  
+		// we can use the method getDataTransferDelay and total delay
+		// getProximityList returns a list of region id's ordered in 
+		// the ascending order of lowest latency
+		InternetCharacteristics vm = null;
+				vm.getInstance();
+		
+		
 	}
 
 }
