f088832e31a717b8c9ac48dbeee6f293e670d8b6
Aniela_Nel <aniela.opolski@gmail.com>
2018-04-06 12:04:43 -0400

    added user delay checks

--- CloudAnalyst/source/atlas/TrafficMonitor.java
+++ CloudAnalyst/source/atlas/TrafficMonitor.java
@@ -8,33 +8,50 @@ import cloudsim.ext.Simulation;
 // this class will monitor interactions between VM instances at single 
 //data centers to find out what the distribution of users is
 import cloudsim.ext.gui.DataCenterUIElement;
+import cloudsim.ext.gui.UserBaseUIElement;
 
 
 
 public class TrafficMonitor {
 
 	private int region;
+	private String name;
 	private InternetCharacteristics internetChar;
 	private List<UserMonitor> users;
 	
+	//create the traffic monitor for the datacenter
 	public TrafficMonitor(DataCenterUIElement dataCenter, Simulation sim) {
 		// general things we can use:
 		this.region = dataCenter.getRegion();
-		
+		this.name = dataCenter.getName();
 		Internet internet = sim.getInternet();
 		internetChar = internet.getInternetChar();
-		//internetChar.getTotalDelay(region, dest, reqSize);
-		
+		//
 		
 	}
 
-	
+	//return the region of the datacenter
 	public int getRegion(){
 		return region;
 	}
-	
+	//add users using this datacenter
 	public void addUser(UserMonitor user){
 		users.add(user);
+	}
+	//remove list of users
+	public void removeAll(){
+		for(Iterator<UserMonitor> um = users.iterator(); um.hasNext();){
+			UserMonitor elem = um.next();
+			um.remove();
+		}
+	}
+	
+	private void getAverageDelays(){
+		double aveDelay = 0;
+		for(Iterator<UserMonitor> um = users.iterator(); um.hasNext();){
+			UserMonitor elem = um.next();
+			aveDelay += internetChar.getTotalDelay(name, elem.getName(), elem.getReqSize());
+		}
 	}
 
 }
