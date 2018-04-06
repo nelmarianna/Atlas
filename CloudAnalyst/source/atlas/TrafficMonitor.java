92a6a7bf1098a0087b2101755d68689f3d86bac3
Aniela_Nel <aniela.opolski@gmail.com>
2018-04-06 11:28:02 -0400

    Internet added to traffic monitor

--- CloudAnalyst/source/atlas/TrafficMonitor.java
+++ CloudAnalyst/source/atlas/TrafficMonitor.java
@@ -2,7 +2,9 @@ package atlas;
 
 import java.util.*;
 
+import cloudsim.ext.Internet;
 import cloudsim.ext.InternetCharacteristics;
+import cloudsim.ext.Simulation;
 // this class will monitor interactions between VM instances at single 
 //data centers to find out what the distribution of users is
 import cloudsim.ext.gui.DataCenterUIElement;
@@ -13,9 +15,13 @@ public class TrafficMonitor {
 
 	protected int region;
 	
-	public TrafficMonitor(DataCenterUIElement dataCenter) {
+	public TrafficMonitor(DataCenterUIElement dataCenter, Simulation sim) {
 		// general things we can use:
 		this.region = dataCenter.getRegion();
+		
+		Internet internet = sim.getInternet();
+		InternetCharacteristics internetChar = internet.getInternetChar();
+		//internetChar.getTotalDelay(region, dest, reqSize);
 		
 		
 	}
